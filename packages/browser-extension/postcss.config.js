c8dac550f03c949f824cdd4bc23a6d3f7d9ce46c
Max Patiiuk <max@patii.uk>
2023-10-22 19:12:52 -0700

    feat(github-markdown-css): Switch theme using className
    Modify github-markdown-css styles to allow for switching theme using
    a light/dark class name.
    
    Posted my solution in https://github.com/sindresorhus/github-markdown-css/issues/104#issuecomment-1774279738

--- packages/browser-extension/postcss.config.js
+++ packages/browser-extension/postcss.config.js
@@ -1,8 +1,43 @@
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-preset-env': {},
-    tailwindcss: {},
-    autoprefixer: {},
+const postcss = require('postcss');
+
+/** @type {import('postcss-load-config').ConfigPlugin} */
+const makeGitHubCssUseClassName = {
+  postcssPlugin: 'makeGitHubCssUseClassName',
+  // When debugging this, https://astexplorer.net/#/2uBU1BLuJ1 is very helpful
+  AtRule: {
+    media(media, { Rule }) {
+      const filePath = media.root().source?.input?.file ?? '';
+      if (!filePath.endsWith('/github-markdown.css')) return;
+
+      const scopeSelector =
+        media.params === '(prefers-color-scheme: dark)'
+          ? '.dark'
+          : media.params === '(prefers-color-scheme: light)'
+          ? '.light'
+          : undefined;
+      if (scopeSelector === undefined) return;
+
+      media.each((child) => {
+        if (child.type !== 'rule') return;
+        const newRule = new Rule({
+          selector: `${scopeSelector} ${child.selector}`,
+        });
+
+        child.each((grandChild) => void newRule.append(grandChild.clone()));
+        media.before(newRule);
+      });
+      media.remove();
+    },
   },
+};
+
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+  plugins: [
+    'postcss-import',
+    'postcss-preset-env',
+    require('tailwindcss'),
+    require('autoprefixer'),
+    makeGitHubCssUseClassName,
+  ],
 };
