57359d663107850441089e732de8b18438751ebf
Max Patiiuk <max@patii.uk>
2023-10-22 19:53:05 -0700

    refactor: Misc minor improvements

--- packages/browser-extension/src/hooks/useDarkMode.tsx
+++ packages/browser-extension/src/hooks/useDarkMode.tsx
@@ -4,5 +4,5 @@ import { useStorage } from './useStorage';
 export function useDarkMode(): boolean {
   const [theme] = useStorage('theme');
   const media = useMedia('(prefers-color-scheme: dark)');
-  return theme === undefined ? media : theme === 'dark';
+  return theme === 'system' ? media : theme === 'dark';
 }
