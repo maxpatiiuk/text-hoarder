1350ce0201420888f085afa452656d69caf22657
Max Patiiuk <max@patii.uk>
2023-10-22 19:14:26 -0700

    feat(ReaderMode): Add base UI

---
+++ packages/browser-extension/src/components/ReaderMode/Dialog.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  SimpleDocument,
+  documentToSimpleDocument,
+} from '../ExtractContent/documentToSimpleDocument';
+import { readerText } from '../../localization/readerText';
+import { H1 } from '../Atoms';
+
+export function Dialog(): JSX.Element {
+  const simpleDocument = React.useMemo(() => documentToSimpleDocument(), []);
+  return (
+    <div className="flex flex-col gap-4 p-4 md:p-16">
+      {simpleDocument === undefined ? (
+        <p>{readerText.noContentFound}</p>
+      ) : (
+        <>
+          <H1>{simpleDocument.title ?? document.title}</H1>
+          <DocumentMeta simpleDocument={simpleDocument} />
+          <Content node={simpleDocument.content} />
+        </>
+      )}
+    </div>
+  );
+}
+
+function DocumentMeta({
+  simpleDocument,
+}: {
+  readonly simpleDocument: SimpleDocument;
+}): JSX.Element {
+  // FEATURE: allow disabling this
+  return <></>;
+}
+
+function Content({ node }: { readonly node: HTMLElement }): JSX.Element {
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+  React.useEffect(() => {
+    const clone = node.cloneNode(true) as Element;
+    containerRef.current?.append(clone);
+    return (): void => clone.remove();
+  }, [node]);
+  return <div className="markdown-body" ref={containerRef} />;
+}
---
+++ packages/browser-extension/src/components/ReaderMode/index.ts
@@ -0,0 +1,77 @@
+/**
+ * This scrip is called by background scrip on keyboard shortcut press - it is
+ * executed in the page that was active when action was triggered.
+ */
+
+import 'github-markdown-css/github-markdown.css';
+import { renderApp } from '../Core/renderApp';
+import { Dialog } from './Dialog';
+
+// Remove previous reader mode instance
+const id = 'text-hoarder-container';
+const previousDialog = document.getElementById(id);
+const alreadyOpen = previousDialog !== null;
+
+if (alreadyOpen) {
+  previousDialog?.remove();
+  restoreBodyScroll();
+} else {
+  // Isolate from parent page's tabindex and scroll
+  const dialog = document.createElement('dialog');
+  dialog.id = id;
+  dialog.style.width = '100vw';
+  dialog.style.height = '100vh';
+  dialog.style.maxWidth = 'none';
+  dialog.style.maxHeight = 'none';
+  dialog.style.margin = '0';
+  dialog.style.padding = '0';
+  dialog.style.position = 'fixed';
+  dialog.style.overflow = 'hidden';
+  dialog.style.border = 'none';
+  dialog.style.outline = 'none';
+
+  // Isolate from parent page's styles
+  const dialogDiv = document.createElement('div');
+  dialogDiv.style.display = 'contents';
+
+  // Can't attach shadow directly to dialog
+  const shadowRoot = dialogDiv.attachShadow({ mode: 'closed' });
+  const container = document.createElement('div');
+  container.classList.add('flex', 'justify-center', 'h-full', 'overflow-auto');
+
+  shadowRoot.append(container);
+  dialog.append(dialogDiv);
+  document.body.append(dialog);
+
+  dialog.showModal();
+  preventBodyScroll();
+  renderApp(container, Dialog, shadowRoot);
+
+  dialog.addEventListener(
+    'close',
+    () => {
+      dialog?.remove();
+      restoreBodyScroll();
+    },
+    { once: true },
+  );
+}
+
+function preventBodyScroll(): void {
+  document.body.style.setProperty('--old-height', document.body.style.height);
+  document.body.style.setProperty(
+    '--old-overflow-y',
+    document.body.style.overflowY,
+  );
+  document.body.style.height = '100vh';
+  document.body.style.overflowY = 'hidden';
+}
+
+function restoreBodyScroll(): void {
+  document.body.style.height =
+    document.body.style.getPropertyValue('--old-height');
+  document.body.style.overflowY =
+    document.body.style.getPropertyValue('--old-overflow-y');
+  document.body.style.setProperty('--old-height', '');
+  document.body.style.setProperty('--old-overflow-y', '');
+}
