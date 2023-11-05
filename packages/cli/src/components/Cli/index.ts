4d0ea06b6b2c878b4793d93d029bf98919dcbf44
Max Patiiuk <max@patii.uk>
2023-11-05 12:37:21 -0800

    refactor(cli): move command descriptions into localization dictionary

--- packages/cli/src/components/Cli/index.ts
+++ packages/cli/src/components/Cli/index.ts
@@ -2,48 +2,39 @@ import { resolve } from 'node:path';
 import { Command } from '@commander-js/extra-typings';
 import { resolveRepositoryPath } from './util';
 import { computeStats } from './stats';
+import { commonText } from '@common/localization/commonText';
+import { cliText } from '@common/localization/cliText';
 
 const program = new Command();
 
-program.name('Text Hoarder').description(
-  `A companion CLI app for the Text Hoarder browser extension. The browser
-extension saves webpages to a GitHub repository. This CLI app then worked
-with the local clone of the GitHub repository to provide insights into your
-most commonly saved webpages. This CLI also has a convenience utility for
-optimizing exported articles for text-to-speech`,
-);
-
-program.showHelpAfterError();
+program
+  .name(commonText.textHoarder)
+  .description(cliText.commandDescription)
+  .showHelpAfterError();
 
 program
   .command('stats')
-  .description('Generate statistics from saved articles')
+  .description(cliText.statsCommandDescription)
   .option(
     '--cwd <path>',
-    'The repository to run the command in. Defaults to current directory',
+    cliText.cwdOptionDescription,
     resolveRepositoryPath,
     process.cwd(),
   )
   .option(
     '--html <path>',
-    'The path at which an HTML file with computed stats would be emitted',
+    cliText.htmlOptionDescription,
     resolve,
     './stats.html',
   )
-  .option(
-    '--no-auto-open',
-    'Do not automatically open the generated HTML file in the browser',
-  )
-  .option(
-    '--no-pull',
-    'Do not automatically try to pull the latest changes from GitHub',
-  )
+  .option('--no-auto-open', cliText.noAutoOpenOptionDescription)
   .option(
     '--json <path>',
-    'The path at which a JSON file with computed stats would be emitted',
+    cliText.jsonOptionDescription,
     resolve,
     './stats.json',
   )
+  .option('--no-pull', cliText.noPullOptionDescription)
   .action(computeStats);
 
 program.parse(process.argv);
--- packages/common/src/localization/cliText.ts
+++ packages/common/src/localization/cliText.ts
@@ -7,5 +7,31 @@ import { dictionary } from './utils';
 // Refer to "Guidelines for Programmers" in ./README.md before editing this file
 
 /* eslint-disable @typescript-eslint/naming-convention */
-export const cliText = dictionary({});
+export const cliText = dictionary({
+  commandDescription: {
+    en: `A companion CLI app for the Text Hoarder browser extension. The browser
+    extension saves webpages to a GitHub repository. This CLI app then worked
+    with the local clone of the GitHub repository to provide insights into your
+    most commonly saved webpages. This CLI also has a convenience utility for
+    optimizing exported articles for text-to-speech`,
+  },
+  statsCommandDescription: {
+    en: 'Generate statistics from saved articles',
+  },
+  cwdOptionDescription: {
+    en: 'The repository to run the command in. Defaults to current directory',
+  },
+  htmlOptionDescription: {
+    en: 'The path at which an HTML file with computed stats would be emitted',
+  },
+  noAutoOpenOptionDescription: {
+    en: 'Do not automatically open the generated HTML file in the browser',
+  },
+  jsonOptionDescription: {
+    en: 'The path at which a JSON file with computed stats would be emitted',
+  },
+  noPullOptionDescription: {
+    en: 'Do not automatically try to pull the latest changes from GitHub',
+  },
+});
 /* eslint-enable @typescript-eslint/naming-convention */
