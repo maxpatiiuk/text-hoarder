cd0de32a3e8aff2122935931fae30be0cde4678b
Max Patiiuk <max@patii.uk>
2024-02-09 20:36:55 -0800

    fix(ReaderMode): fix missing scroll bars for pre in fieldset

--- packages/browser-extension/src/components/Core/styleLoader.ts
+++ packages/browser-extension/src/components/Core/styleLoader.ts
@@ -1,3 +1,9 @@
+/**
+ * Rather than appending styles directly to the page (as that would mess with)
+ * the host page's styles, append styles to an array, so that they can later be
+ * manually appended to Shadow DOM
+ */
+
 const styleTags: HTMLLinkElement[] = [];
 export const getStyleTags = () => styleTags.map((node) => node.cloneNode(true));
 
